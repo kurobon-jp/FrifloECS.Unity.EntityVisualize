bfebac5e974f056836242a42353a8f52e48ce83a
kurobon <kurobon@gmail.com>
2025-10-07 12:51:11 +0900

    Supported delete entity

--- Assets/FrifloECS.Unity.EntityVisualize/Editor/EntityInspector.cs
+++ Assets/FrifloECS.Unity.EntityVisualize/Editor/EntityInspector.cs
@@ -25,12 +25,7 @@ namespace FrifloECS.Unity.EntityVisualize.Editor
             _window = (EntitiesHierarchyWindow)target;
             _entityInfo = _window.GetSelectedEntityInfo();
             if (_entityInfo == null) return;
-            var style = new GUIStyle(EditorStyles.boldLabel)
-            {
-                fontSize = 24,
-                padding = new RectOffset(8, 8, 8, 8)
-            };
-            GUILayout.Label($"{_entityInfo}", style);
+            DrawCustomHeader($"{_entityInfo}");
         }
 
         /// <summary>
@@ -40,6 +35,41 @@ namespace FrifloECS.Unity.EntityVisualize.Editor
         {
             if (_entityInfo == null) return;
             _entityInfo.OnInspectorGUI();
+            EditorUtility.SetDirty(target);
+        }
+
+        private void DrawCustomHeader(string title)
+        {
+            var rect = GUILayoutUtility.GetRect(1, 48);
+            rect.xMin = 0;
+            rect.xMax += 4;
+            EditorGUI.DrawRect(rect,  new(0.18f, 0.18f, 0.18f));
+            var style = new GUIStyle(EditorStyles.boldLabel)
+            {
+                fontSize = 24,
+                padding = new RectOffset(8, 8, 8, 8)
+            };
+            var labelRect = new Rect(rect.x + 6, rect.y + 2, rect.width - 40, rect.height);
+            EditorGUI.LabelField(labelRect, title, style);
+            var menuRect = new Rect(rect.xMax - 25, rect.y + 2, 20, 20);
+            if (GUI.Button(menuRect, EditorGUIUtility.IconContent("_Menu"), GUIStyle.none))
+            {
+                ShowHeaderMenu();
+            }
+        }
+
+        private void ShowHeaderMenu()
+        {
+            var menu = new GenericMenu();
+            menu.AddItem(new GUIContent("Delete Entity"), false, DeleteEntity);
+            menu.ShowAsContext();
+        }
+
+        private void DeleteEntity()
+        {
+            if (_entityInfo == null) return;
+            _entityInfo.Entity.DeleteEntity();
+            _entityInfo = null;
             EditorUtility.SetDirty(target);
         }
     }
--- Assets/FrifloECS.Unity.EntityVisualize/Editor/Models/EntityInfoEditor.cs
+++ Assets/FrifloECS.Unity.EntityVisualize/Editor/Models/EntityInfoEditor.cs
@@ -12,6 +12,44 @@ namespace FrifloECS.Unity.EntityVisualize.Editor.Models
         public void OnInspectorGUI()
         {
             var color = 0;
+            if (Components.Count > 0)
+            {
+                using (new GUILayout.VerticalScope("box"))
+                {
+                    GUILayout.Label("Components", EditorStyles.boldLabel);
+                }
+
+                using (new GUILayout.VerticalScope())
+                {
+                    for (var i = 0; i < Components.Count; i++)
+                    {
+                        var componentInfo = Components[i];
+                        var componentType = componentInfo.EntityComponent.Type;
+                        ComponentFoldouts.TryGetValue(componentType, out var foldout);
+                        var rect = EditorGUILayout.GetControlRect();
+                        var width = rect.width;
+                        EditorGUI.DrawRect(rect, GetRainbowColor(color++));
+                        rect.x += 15;
+                        rect.width -= 35;
+                        foldout = EditorGUI.Foldout(rect, foldout, componentInfo.ComponentName, true);
+                        ComponentFoldouts[componentType] = foldout;
+                        if (foldout)
+                        {
+                            EditorGUI.indentLevel++;
+                            componentInfo.OnInspectorGUI(Entity);
+                            EditorGUI.indentLevel--;
+                        }
+
+                        rect.width = 25;
+                        rect.x = width - 10;
+                        if (GUI.Button(rect, "-", EditorStyles.miniButton))
+                        {
+                            EntityUtils.RemoveEntityComponent(Entity, componentType);
+                        }
+                    }
+                }
+            }
+
             if (Tags.Count > 0)
             {
                 using (new GUILayout.VerticalScope("box"))
@@ -19,9 +57,8 @@ namespace FrifloECS.Unity.EntityVisualize.Editor.Models
                     GUILayout.Label("Tags", EditorStyles.boldLabel);
                 }
 
-                using (new GUILayout.HorizontalScope())
+                using (new GUILayout.VerticalScope())
                 {
-                    // GUILayout.Space(15);
                     for (var i = 0; i < Tags.Count; i++)
                     {
                         var tag = Tags[i];
@@ -41,48 +78,6 @@ namespace FrifloECS.Unity.EntityVisualize.Editor.Models
                 }
             }
 
-            if (Components.Count > 0)
-            {
-                using (new GUILayout.VerticalScope("box"))
-                {
-                    GUILayout.Label("Components", EditorStyles.boldLabel);
-                }
-
-                using (new GUILayout.HorizontalScope())
-                {
-                    // GUILayout.Space(15);
-                    using (new GUILayout.VerticalScope())
-                    {
-                        for (var i = 0; i < Components.Count; i++)
-                        {
-                            var componentInfo = Components[i];
-                            var componentType = componentInfo.EntityComponent.Type;
-                            ComponentFoldouts.TryGetValue(componentType, out var foldout);
-                            var rect = EditorGUILayout.GetControlRect();
-                            var width = rect.width;
-                            EditorGUI.DrawRect(rect, GetRainbowColor(color++));
-                            rect.x += 15;
-                            rect.width -= 35;
-                            foldout = EditorGUI.Foldout(rect, foldout, componentInfo.ComponentName, true);
-                            ComponentFoldouts[componentType] = foldout;
-                            if (foldout)
-                            {
-                                EditorGUI.indentLevel++;
-                                componentInfo.OnInspectorGUI(Entity);
-                                EditorGUI.indentLevel--;
-                            }
-
-                            rect.width = 25;
-                            rect.x = width - 10;
-                            if (GUI.Button(rect, "-", EditorStyles.miniButton))
-                            {
-                                EntityUtils.RemoveEntityComponent(Entity, componentType);
-                            }
-                        }
-                    }
-                }
-            }
-
             if (Scripts.Count > 0)
             {
                 using (new GUILayout.VerticalScope("box"))
@@ -90,9 +85,8 @@ namespace FrifloECS.Unity.EntityVisualize.Editor.Models
                     GUILayout.Label("Scripts", EditorStyles.boldLabel);
                 }
 
-                using (new GUILayout.HorizontalScope())
+                using (new GUILayout.VerticalScope())
                 {
-                    // GUILayout.Space(15);
                     for (var i = 0; i < Scripts.Count; i++)
                     {
                         var script = Scripts[i];
